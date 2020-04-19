127b381e42372c643abc29e1e8cf57c04039af20
Diya <diya.rajon@yahoo.com>
2020-04-19 01:17:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var myObj = JSON.parse(this.responseText);
-    document.getElementById("demo").innerHTML = myObj.name;
+    document.getElementById("demo").innerHTML = myObj;
   }
 };
 xmlhttp.open("GET", "side.json", true);
